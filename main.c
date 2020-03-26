b18f44ba4a7f5636eaeb728eead04ad71d44b2f7
marco augusto <marcoaraujo96@gmail.com>
2020-03-26 16:01:28 +0000

    Add existing file

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include "table.h"
 
-int main(){
+int main()
+{
 
-     printf("\033[01;33m");  // bold yellow
+     printf("\033[01;33m"); // bold yellow
      printf("     ____        _   _   _           _     _                  \n");
      printf("    | __ )  __ _| |_| |_| | ___  ___| |__ (_)_ __             \n");
      printf("    |  _ \\ / _` | __| __| |/ _ \\/ __| '_ \\| | '_ \\            \n");
@@ -13,28 +14,47 @@ int main(){
      printf("                                            |_|               \n");
      printf("\033[0m");
 
-
      //  ------------- teste ----------------
-      struct Ship* barco = newShip(1);
-      struct Cell* matriz[10][10];
-      int c = 0;
-     for(int i = 0; i < 10; i++){
-          for(int j = 0; j < 10; j++){
+     Ship *barco = newShip(1);
+     Player p1;
+     Player p2;
+     puts("primeiro jogador escolhendo navios");
+     chooseShips(p1);
+     puts("segundo  jogador escolhendo navios");
+     chooseShips(p2);
+     setUp(10, p1.matriz);
+     setUp(10, p2.matriz);
 
-               matriz[i][j] = newCell();
-          }
-     }
-     for(int i = 0; i < 10; i++){
-          updateCell(matriz[i][0],1,barco);
+     for (int i = 0; i < 10; i++)
+     {
+          updateCell(&p1.matriz[i][0], 1, barco);
      }
 
-     for(int i = 0; i < 10; i++){
-          for(int j = 0; j < 10; j++){
-               printf("%d",matriz[i][j]->state);
+     for (int i = 0; i < 10; i++)
+     {
+          for (int j = 0; j < 10; j++)
+          {
+               printf("%d", p1.matriz[i][j].state);
           }
           printf("\n");
      }
      // ----------------- / / -------------------------
 
      return 0;
+}
+
+void chooseShips(Player player)
+{
+     int amount = 0, size = 0;
+     size = 5;
+     player.myShips = malloc(sizeof(Node) * size);
+
+     for (int i = 1; i <= size; i++)
+     {
+          printf("Escolha a quantidade de barcos do tipo  %d \n", i);
+          scanf("%d", &amount);
+          player.myShips[i].value.type = i;
+          player.myShips[i].amount = amount;
+          player.myShips[i].index = i;
+     }
 }
--- table.h
+++ table.h
@@ -4,28 +4,43 @@
 //  //vai ter de ser criada
 // } Player;
 
-
 typedef struct Ship
 {
 
-  int type;  //int ou string(char*) ?
+  int type; //int ou string(char*) ?
   int ActiveCells;
 
 } Ship;
 
 typedef struct Cell
 {
-     int state;             // 0 = empty, 1 = piece without being hitted,
-                            // 2 = piece hitted, 3 = missed shot
-     int shot_counter;
-     Ship* bellongsTo;      // if cell is empty bellongsTo is null
+  int state; // 0 = empty, 1 = piece without being hitted,
+             // 2 = piece hitted, 3 = missed shot
+  int shot_counter;
+  Ship *bellongsTo; // if cell is empty bellongsTo is null
 } Cell;
+typedef struct Node
+{
+  Ship value;
+  int amount;
+  int index;
+} Node;
 
+typedef struct Player
+{
+  int ID_P;
+  char *name;
+  Node *myShips;
+  Cell matriz[10][10];
+} Player;
 
-void updateCell(Cell*,int, Ship*);
-Cell* newCell();
-int isFree(Cell* );
-void shotCell(Cell* );
-void decreaseActiveCell(Ship* );
-Ship* newShip(int );
+void chooseShips(Player player);
+
+void updateCell(Cell *, int, Ship *);
+Cell *newCell();
+int isFree(Cell *);
+void shotCell(Cell *);
+void decreaseActiveCell(Ship *);
+Ship *newShip(int);
 void shipSank(Ship ship); //condiçao para ver se ainda ha barcos
+void setUp(int size, Cell matriz[10][10]);
